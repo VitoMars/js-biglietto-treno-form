d8daf61fa56dd82ae08782620788192431a6bff4
Vito <vito95v@gmail.com>
2021-07-22 15:44:42 +0200

    Creazione primo banner ed esempio del box

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var btnGenera = document.getElementById("calcolaPrezzo");
+console.log(btnGenera);
+
+btnGenera.addEventListener("click", function () {
+  var nome = document.getElementById("nome").value;
+  var km = document.getElementById("km").value;
+  var fasciaEta = document.getElementById("fascia-eta").value;
+
+  var biglietto = document.getElementById("biglietto");
+  biglietto.style.display = "block";
+});
+
+var btnAnnulla = document.getElementById("annulla");
+
+btnAnnulla.addEventListener("click", function () {
+  var biglietto = document.getElementById("biglietto");
+  biglietto.style.display = "none";
+
+  // Svuoto gli input
+  document.getElementById("nome").value = "";
+  document.getElementById("km").value = "";
+  document.getElementById("fascia-eta").value = "";
+});
