eb8bd1467f58f337b73147a6d12da33056f25a33
Vito <vito95v@gmail.com>
2021-07-22 17:36:23 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -5,8 +5,27 @@ btnGenera.addEventListener("click", function () {
   var nome = document.getElementById("nome").value;
   var km = document.getElementById("km").value;
   var fasciaEta = document.getElementById("fascia-eta").value;
+  var prezzoBiglietto = km * 0.21;
+  var prezzoFinale;
 
-  document.getElementById("nomeValue").innerHTML = "ciao";
+  if (fasciaEta == "minorenne") {
+    prezzoFinale = prezzoBiglietto - prezzoBiglietto * (20 / 100); //Sconto del 20%
+  } else if (fasciaEta == "over65") {
+    prezzoFinale = prezzoBiglietto - prezzoBiglietto * (40 / 100); //Sconto del 40%
+  } else {
+    prezzoFinale = prezzoBiglietto; //Nessuno sconto
+  }
+
+  document.getElementById("nomeValue").innerHTML = nome;
+  document.getElementById("offerta").innerHTML = "Sconto " + fasciaEta;
+  document.getElementById("carrozza").innerHTML = Math.round(
+    Math.random() * 100
+  );
+  document.getElementById("codiceCP").innerHTML = Math.round(
+    Math.random() * 99999
+  );
+  document.getElementById("prezzoBiglietto").innerHTML =
+    prezzoFinale.toFixed(2);
 
   var biglietto = document.getElementById("biglietto");
   biglietto.style.display = "block";
